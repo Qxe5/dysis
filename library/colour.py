11bec3fe55b3e640ff6992190d1dd5a0953a383c
Qxe5 <cypherspace@icloud.com>
2022-02-09 03:38:02 +0000

    add colours

---
+++ library/colour.py
@@ -0,0 +1,13 @@
+'''Embed colours'''
+colours = {
+        'black'      : 0x000000,
+        'dark blue'  : 0x00008b,
+        'gray'       : 0xc0c0c0,
+        'green'      : 0x1d9e74,
+        'light blue' : 0x9db5cc,
+        'orange'     : 0xff8b53,
+        'purple'     : 0xbc5a84,
+        'violet'     : 0xa086b7,
+        'white'      : 0xcccccc,
+        'yellow'     : 0xfde68a
+}
