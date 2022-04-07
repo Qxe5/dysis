3cc95122472028088dda1ac7cb20e72eaab8e0fd
Qxe5 <cypherspace@icloud.com>
2022-04-07 08:23:37 +0100

    add entry point

---
+++ bot.py
@@ -0,0 +1,55 @@
+'''Entry point'''
+from getpass import getpass
+import logging
+from signal import signal, SIGINT
+import sys
+
+import discord
+from discord.ext import tasks, pages
+
+from library import cards
+from library.search import autocomplete, lookup
+
+signal(SIGINT, lambda signalnumber, stackframe: sys.exit())
+logging.basicConfig()
+
+# init
+bot = discord.Bot()
+
+@bot.listen()
+async def on_ready():
+    '''Print info when ready'''
+    print('Logged in as', bot.user)
+
+# tasks
+@tasks.loop(hours=24)
+async def updatecards():
+    '''Update lookup table'''
+    await cards.generatecards()
+
+updatecards.start()
+
+# commands
+@bot.slash_command(name='cg')
+async def search(
+    ctx,
+    card : discord.Option(str, 'Card name:', autocomplete=autocomplete)
+):
+    '''Search for a TCG/OCG card'''
+    if results := await lookup(card):
+        paginator = pages.Paginator(results)
+        await paginator.respond(ctx.interaction)
+    else:
+        await ctx.respond(f'No cards found for search `{card}`', ephemeral=True)
+
+@bot.slash_command()
+async def servers(ctx):
+    '''Get the server count of the bot'''
+    await ctx.respond(f'{len(bot.guilds)} Servers', ephemeral=True)
+
+# authenticate
+try:
+    bot.run(getpass('Token: '))
+except discord.LoginFailure as loginfailure:
+    print('Invalid Token')
+    raise SystemExit(1) from loginfailure
