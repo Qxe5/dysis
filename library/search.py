0c5e1b6597caafc4acab3f09d5c7b0c09b43bbbf
Qxe5 <cypherspace@icloud.com>
2022-04-12 21:29:47 +0100

    add contains to autocomplete

--- library/search.py
+++ library/search.py
@@ -11,7 +11,8 @@ async def autocomplete(ctx):
     '''Return autocompletions from a current search term'''
     term = ctx.options['card']
 
-    return await fuzzy(term, [card for card in cards if card.startswith(term[0])]) if term else []
+    return await fuzzy(term, [card for card in cards if card.startswith(term[0]) or term in card]) \
+           if term else []
 
 async def lookup(term):
     '''Lookup a search term in the lookup table and return the closest matches or None'''
