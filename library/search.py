5fbc0ac4367df49f584129631b2db0ee83ded985
Qxe5 <cypherspace@icloud.com>
2022-04-16 16:15:30 +0100

    change keyword params

--- library/search.py
+++ library/search.py
@@ -3,7 +3,9 @@ from difflib import get_close_matches
 
 from library.cards import cards
 
-async def fuzzy(comparate, comparables, results=25):
+DEFAULT_RESULTS = 25
+
+async def fuzzy(comparate, comparables, results=DEFAULT_RESULTS):
     '''Compare the comparate with the comparables and return results'''
     return get_close_matches(comparate, comparables, cutoff=0.1, n=results)
 
@@ -14,7 +16,7 @@ async def autocomplete(ctx):
     return await fuzzy(term, [card for card in cards if card.startswith(term[0]) or term in card]) \
            if term else []
 
-async def lookup(term):
-    '''Lookup a search term in the lookup table and return the closest matches or None'''
-    if matches := await fuzzy(term.lower(), cards.keys()):
+async def lookup(term, results=DEFAULT_RESULTS):
+    '''Lookup a search term in the lookup table and return the closest results or None'''
+    if matches := await fuzzy(term.lower(), cards.keys(), results):
         return [cards[match] for match in matches]
