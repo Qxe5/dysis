2d795ee0f759dfbd6ca52d3cff54364dff854308
Qxe5 <cypherspace@icloud.com>
2022-04-22 21:49:44 +0100

    add request and parsing for KOIDs

--- library/cards.py
+++ library/cards.py
@@ -3,26 +3,37 @@ import asyncio
 
 import aiohttp
 
-from library.card import Card
+from library.card import YGORGAPI, koids, Card
 
 cards = {}
 
 async def retrieve_card_data():
     '''Retrieve and return raw card data or None on error'''
-    api = ('https://db.ygoprodeck.com/api/v7/cardinfo.php?'
-           'misc=yes')
+    ygorg = f'{YGORGAPI}data/idx/card/name/en'
+    ygopro = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?misc=yes'
 
     async with aiohttp.ClientSession(raise_for_status=True) as client:
         try:
-            async with client.get(api) as response:
-                return (await response.json())['data']
+            async with client.get(ygorg) as ygorgresponse, client.get(ygopro) as ygoproresponse:
+                return (await ygorgresponse.json(), (await ygoproresponse.json())['data'])
         except (aiohttp.ClientConnectionError, aiohttp.ClientResponseError, asyncio.TimeoutError):
             return None
 
 async def generatecards():
     '''Update lookup table'''
     if card_data := await retrieve_card_data():
+        koids.clear()
         cards.clear()
+
+        koid_data, card_data = card_data
+
+        for card, kids in koid_data.items():
+            koids[card] = max(kids)
+
+            if len(kids) > 1:
+                koids[f'{card} (Skill Card)'] = min(kids)
+
+        koids.update({koid : card for card, koid in koids.items()})
 
         for card_datum in card_data:
             card = Card(card_datum)
