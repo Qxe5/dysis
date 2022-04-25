1f8ce213c4105954fe39da50242d703871cc6b4e
Qxe5 <cypherspace@icloud.com>
2022-04-25 04:09:59 +0100

    subclass Paginator

---
+++ library/pagination.py
@@ -0,0 +1,15 @@
+'''Pagination'''
+from contextlib import suppress
+
+from discord import NotFound
+from discord.ext import pages
+
+class Paginator(pages.Paginator):
+    '''A representation of a paginator'''
+    async def on_timeout(self):
+        '''Disable the buttons'''
+        for button in self.children:
+            button.disabled = True
+
+        with suppress(NotFound):
+            await self.message.edit(view=self)
