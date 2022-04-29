c83d550774b38df63b5ac577b90d4e5d47c36c80
Qxe5 <cypherspace@icloud.com>
2022-04-29 05:40:53 +0100

    encapsulate common command elements

---
+++ library/helper.py
@@ -0,0 +1,17 @@
+'''Command helper'''
+from discord import Option, Member
+
+from library.search import autocomplete
+
+cardoption = Option(str, 'Card name:', autocomplete=autocomplete)
+mentionoption = Option(Member, 'Mention:', default=None)
+publicoption = Option(bool, 'Should the results be visible to everyone?', default=True)
+
+async def ping(ctx, member):
+    '''Mention a member'''
+    if member:
+        await ctx.respond(member.mention)
+
+async def noresult(ctx, term):
+    '''Handle a lack of search results given a search term'''
+    await ctx.respond(f'No cards found for search `{term}`', ephemeral=True)
