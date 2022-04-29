8c909b973be34f08c65cff4a68d92552952cfdc3
Qxe5 <cypherspace@icloud.com>
2022-04-29 05:30:47 +0100

    add API endpoints

--- library/api.py
+++ library/api.py
@@ -1,3 +1,6 @@
 '''API endpoints'''
+from string import Template
+
 YGOPRO = 'https://db.ygoprodeck.com/api/v7/'
+ART = Template('https://storage.googleapis.com/ygoprodeck.com/pics_artgame/$cid.jpg')
 YGORG = 'https://db.ygorganization.com/'
