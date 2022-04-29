e131aff7d12f118ca1cbdf433309a319b86a954b
Qxe5 <cypherspace@icloud.com>
2022-04-29 06:23:28 +0100

    tie in entry point

--- bot.py
+++ bot.py
@@ -8,9 +8,9 @@ import discord
 from discord.ext import tasks
 
 from cogs.status import Status
-from library import cards
+from library import cards, helper
 from library.pagination import Paginator
-from library.search import autocomplete, lookup
+from library.search import lookup
 
 signal(SIGINT, lambda signalnumber, stackframe: sys.exit())
 logging.basicConfig()
@@ -32,34 +32,29 @@ async def updatecards():
 updatecards.start()
 
 # commands
-cardoption = discord.Option(str, 'Card name:', autocomplete=autocomplete)
-mentionoption = discord.Option(discord.Member, 'Mention:', default=None)
-publicoption = discord.Option(bool, 'Should the results be visible to everyone?', default=True)
-
 @bot.slash_command()
 async def search(
     ctx,
-    card : cardoption,
-    mention : mentionoption,
-    public : publicoption
+    card : helper.cardoption,
+    mention : helper.mentionoption,
+    public : helper.publicoption
 ):
     '''Search for TCG/OCG/Skill cards'''
     if results := await lookup(card):
         paginator = Paginator([await result.make_embed() for result in results])
         await paginator.respond(ctx.interaction, ephemeral=not public)
-        if mention:
-            await ctx.respond(mention.mention)
+        await helper.ping(ctx, mention)
     else:
-        await ctx.respond(f'No cards found for search `{card}`', ephemeral=True)
+        await helper.noresult(ctx, card)
 
 @bot.slash_command()
 async def rulings( # pylint: disable=too-many-arguments
     ctx,
-    card : cardoption,
+    card : helper.cardoption,
     question : discord.Option(str, 'Keywords:', default=''),
     index : discord.Option(int, name='qa', description='YGOrg Q&A ID:', default=0),
-    mention : mentionoption,
-    public : publicoption
+    mention : helper.mentionoption,
+    public : helper.publicoption
 ):
     '''Search for rulings'''
     if result := await lookup(card, results=1):
@@ -69,12 +64,28 @@ async def rulings( # pylint: disable=too-many-arguments
         if results := await result.getrulings(question, index):
             paginator = Paginator(results)
             await paginator.respond(ctx.interaction, ephemeral=not public)
-            if mention:
-                await ctx.respond(mention.mention)
+            await helper.ping(ctx, mention)
         else:
             await ctx.respond(f'`{result.name}` has no current rulings', ephemeral=not public)
     else:
-        await ctx.respond(f'No card found for search `{card}`', ephemeral=True)
+        await helper.noresult(ctx, card)
+
+@bot.slash_command()
+async def arts(
+    ctx,
+    card : helper.cardoption,
+    mention : helper.mentionoption,
+    public : helper.publicoption
+):
+    '''Search for card arts'''
+    if result := await lookup(card, results=1):
+        result = result.pop()
+
+        paginator = Paginator(await result.make_image_embeds())
+        await paginator.respond(ctx.interaction, ephemeral=not public)
+        await helper.ping(ctx, mention)
+    else:
+        await helper.noresult(ctx, card)
 
 @bot.slash_command()
 async def servers(ctx):
