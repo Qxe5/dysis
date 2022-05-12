e8ece53d5256733b3a2c17ec8213eb317cdfafb0
Qxe5 <cypherspace@icloud.com>
2022-05-12 21:39:39 +0100

    add ygoprices endpoint

--- library/api.py
+++ library/api.py
@@ -3,4 +3,5 @@ from string import Template
 
 YGOPRO = 'https://db.ygoprodeck.com/api/v7/'
 ART = Template('https://storage.googleapis.com/ygoprodeck.com/pics_artgame/$cid.jpg')
+YGOPRICES = 'https://yugiohprices.com/api/set_image/'
 YGORG = 'https://db.ygorganization.com/'
