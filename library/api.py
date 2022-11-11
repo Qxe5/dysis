88adfea1c6f75bdcec7594bae1a80f6224905ca4
Qxe5 <cypherspace@icloud.com>
2022-11-11 02:32:33 +0000

    change endpoint

--- library/api.py
+++ library/api.py
@@ -2,6 +2,6 @@
 from string import Template
 
 YGOPRO = 'https://db.ygoprodeck.com/api/v7/'
-ART = Template('https://storage.googleapis.com/ygoprodeck.com/pics_artgame/$cid.jpg')
+ART = Template('https://images.ygoprodeck.com/images/cards_cropped/$cid.jpg')
 YGOPRICES = 'https://yugiohprices.com/api/'
 YGORG = 'https://db.ygorganization.com/'
